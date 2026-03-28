6c4fc6b88fcb2a2a86c1c44abe9d20bf66ba7df1
loveil381 <qq381490307@gmail.com>
2026-03-28 21:04:43 +0900

    fix(core): remove zone-wrapped app startup

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:hananote/app/app.dart';
 import 'package:hananote/app/di/injection.dart';
-import 'package:hananote/core/error/error_boundary.dart';
 import 'package:hananote/core/notifications/notification_service.dart';
 import 'package:timezone/data/latest_all.dart' as tz;
 
 void main() async {
-  await ErrorBoundary.init(() async {
-    WidgetsFlutterBinding.ensureInitialized();
-    tz.initializeTimeZones();
-    configureDependencies();
+  WidgetsFlutterBinding.ensureInitialized();
+  tz.initializeTimeZones();
+  configureDependencies();
+
+  try {
     await getIt<NotificationService>().init();
     await getIt<NotificationService>().requestPermissions();
-    ErrorBoundary.markAppStarted();
-    runApp(const HanaNote());
-  });
+  } catch (e) {
+    debugPrint('[main] Notification init error (non-fatal): $e');
+  }
+
+  runApp(const HanaNote());
 }
